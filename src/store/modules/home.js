d6acf4898499818d9c4157a650fde1cecf1c40f1
lyp <1183391880@qq.com>
2019-12-03 21:02:24 +0800

    clist

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,15 +1,20 @@
 import {getMasterBrandList} from '@/services/index'
-
 const state = {
-    list: []
+    list: [],
+    lis:[]
 }
-
 const mutations = {
-    updateList(state, payload){
+    updateList(state, payload){   
         state.list = payload;
-    }
-}
-
+        state.list.map(item=>{
+            let letter = item.Spelling[0];
+            let arr = state.list.filter(item=>item.Spelling[0]===letter)
+            if(state.lis.findIndex(item=>item.letter==letter)===-1){
+                    state.lis.push({letter,arr})
+            }
+        })   
+     }
+ }
 const actions = {
     async getMasterBrandList({commit}, payload){
         let res = await getMasterBrandList();
@@ -17,7 +22,6 @@ const actions = {
         commit('updateList', res.data);
     }
 }
-
 export default {
     namespaced: true,
     state,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+          <div v-for="(item,index) in lis" :key="index">
+             <p class="tit">{{item.letter}}</p>
+                 <ul v-for="(item,index) in item.arr" :key="index">
+                   <li><img :src="item.CoverPhoto" alt=""></li>
+                   <li><span>{{item.Name}}</span> </li>
+                 </ul>
+          </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import {mapActions, mapState} from 'vuex'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+          
   },
   computed: {
     ...mapState({
-      list: state=>state.home.list
+      list: state=>state.home.list,
+      lis: state=>state.home.lis,
     })
   },
   methods: {
@@ -32,3 +36,36 @@ export default {
   }
 }
 </script>
+<style lang="scss" scrped>
+  *{
+    margin: 0px;
+    padding: 0;
+    list-style: none;
+    box-sizing: border-box;
+    text-decoration: none
+  }
+   ul{
+      width: 100%;
+      height: 50px;
+      display: flex;
+      border-bottom: 1px solid #ddd;
+   }
+  li img{
+     width: .8rem;
+     height: .8rem;
+     margin-top: 5px
+   }
+   li span{
+    font-size: .32rem;
+    margin-left: .4rem;
+    line-height: 50px;
+    
+   }
+   .tit{
+      font-size: .28rem;
+      line-height: .4rem;
+      background: #f4f4f4;
+      padding-left: .3rem;
+      color: #aaa;
+   }
+</style>
