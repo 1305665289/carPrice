a5924da2b71d5d19595d127d52f79bb23ba05c38
lyp <1183391880@qq.com>
2019-12-05 13:44:26 +0800

    new

--- src/services/modules/home.js
+++ src/services/modules/home.js
@@ -1,6 +1,5 @@
 import request from '@/utils/request';
 
-<<<<<<< HEAD
 // 获取首页品牌列表
 export let getMasterBrandList = ()=>{
     return request('/v2-car-getMasterBrandList.html')
@@ -11,10 +10,4 @@ export let getMasterRightList = (MasterID)=>{
 }
 export let getDetailList = (SerialID)=>{
     return request('/v2-car-getInfoAndListById.html',{params:{SerialID}})
-=======
-
-//获取详情页数据
-export let getDetailList=()=>{
-    return request('/v2-car-getInfoAndListById.html',{params:{SerialID:2593}})
->>>>>>> szw
 }
